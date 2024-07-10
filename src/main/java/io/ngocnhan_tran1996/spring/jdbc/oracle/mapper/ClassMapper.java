14415fc35410a7a2596cdaf2cf46736095346268
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2024-07-10 19:29:50 +0700

    act: init interface

--- src/main/java/io/ngocnhan_tran1996/spring/jdbc/oracle/mapper/ClassMapper.java
+++ src/main/java/io/ngocnhan_tran1996/spring/jdbc/oracle/mapper/ClassMapper.java
@@ -6,12 +6,12 @@ public abstract class ClassMapper<T> extends AbstractMapper<T> {
 
     private Class<T> mappedClass;
 
-    public Class<T> getMappedClass() {
+    protected Class<T> getMappedClass() {
 
         return this.mappedClass;
     }
 
-    public void setMappedClass(Class<T> mappedClass) {
+    protected void setMappedClass(Class<T> mappedClass) {
 
         this.mappedClass = Objects.requireNonNull(mappedClass, "mapped class");
     }
