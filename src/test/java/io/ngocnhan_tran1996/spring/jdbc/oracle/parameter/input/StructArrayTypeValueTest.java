6685efd93312211e9891e978d851ff4a06ddeae1
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2024-07-13 21:06:31 +0700

    test: improve testcase

---
+++ src/test/java/io/ngocnhan_tran1996/spring/jdbc/oracle/parameter/input/StructArrayTypeValueTest.java
@@ -0,0 +1,48 @@
+package io.ngocnhan_tran1996.spring.jdbc.oracle.parameter.input;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.zaxxer.hikari.HikariDataSource;
+import io.ngocnhan_tran1996.spring.jdbc.oracle.Customer;
+import io.ngocnhan_tran1996.spring.jdbc.oracle.SetupTestData;
+import io.ngocnhan_tran1996.spring.jdbc.oracle.config.ExampleConfig;
+import io.ngocnhan_tran1996.spring.jdbc.oracle.mapper.DelegateMapper;
+import java.sql.SQLException;
+import oracle.jdbc.OracleConnection;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+@Import(ExampleConfig.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class StructArrayTypeValueTest extends SetupTestData {
+
+    @Autowired
+    HikariDataSource dataSource;
+
+    @Test
+    void create_type_value_with_values_is_null() throws SQLException {
+
+        var connection = dataSource.getConnection();
+        var output = new StructArrayTypeValue<>(
+            "EXAMPLE_PACK.CUSTOMERS",
+            null,
+            "EXAMPLE_PACK.CUSTOMERS",
+            DelegateMapper.newInstance(Customer.class)
+        )
+            .createTypeValue(connection, "EXAMPLE_PACK.CUSTOMERS");
+
+        var expectOutput = connection
+            .unwrap(OracleConnection.class)
+            .createOracleArray("EXAMPLE_PACK.CUSTOMERS", null);
+
+        assertThat(output)
+            .usingRecursiveComparison()
+            .isEqualTo(expectOutput);
+    }
+
+}
---
+++ src/test/java/io/ngocnhan_tran1996/spring/jdbc/oracle/parameter/output/ArrayReturnTypeTest.java
@@ -0,0 +1,21 @@
+package io.ngocnhan_tran1996.spring.jdbc.oracle.parameter.output;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ArrayReturnTypeTest {
+
+    @Test
+    void convert_struct_always_throw_exception() {
+
+        var returnType = new ArrayReturnType<>();
+
+        assertThatExceptionOfType(UnsupportedOperationException.class)
+            .isThrownBy(() -> returnType.convertStruct(null, null));
+    }
+
+}
